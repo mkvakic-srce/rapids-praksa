32f631e0d3470a2c2f96cb98b63d8a2276dc20fa
Marko Kvakic <mkvakic@teran.srce.hr>
2022-06-09 13:32:26 +0200

    zadatak2 update

--- zadatak2/double-mpi4py.py
+++ zadatak2/double-mpi4py.py
@@ -1,47 +1,15 @@
 # podignuti mpi4py modul
-from mpi4py import MPI
 
 # pomnozi listu brojeva s dva
-def double(numbers):
-    doubled = []
-    for number in numbers:
-        double = number*2
-        doubled.append(double)
-    return doubled
 
-# pozvati hello funkciju ovisno o broju dostupnih jezgri
 if __name__ == '__main__':
 
     # inicijaliziraj MPI
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
 
     # definiraj listu brojeva ovisno o rangu
-    if rank == 0:
-        n = 10**8
-        numbers = list(range(n))
-    else:
-        numbers = None
 
     # razdijeli i raspodijeli listu brojeva na ostale jezgre putem scatter
-    if rank == 0:
-        sublists = []
-        for i in range(size):
-            stride = n//size
-            start = i*stride
-            end = start + stride
-            sublists.append(numbers[start:end])
-    else:
-        sublists = None
-    sublist = comm.scatter(sublists, root=0)
 
     # izracunaj listu udvostrucenih brojeva i spoji ih nazad pomocu gather
-    sublist_doubled = double(sublist)
-    sublists_doubled = comm.gather(sublist_doubled, root=0)
 
-    # print
-    if comm.rank == 0:
-        doubled = [ doubled_number for sublist_doubled in sublists_doubled for doubled_number in sublist_doubled ]
-        print('numbers : %s' % numbers[:5])
-        print('doubled : %s' % doubled[:5])
+    # isprintaj prvih pet brojeva u originalnoj i udvostrucenoj listi
--- zadatak2/install-mpi4py.sh
+++ zadatak2/install-mpi4py.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# pripremiti knjižnicu za MPI
-module load mpi/openmpi41-x86_64
+# podignuti knjižnicu za MPI
 
 # stvoriti virtualno okruženje za mpi4py
-python3 -m venv --system-site-packages venv-mpi4py
 
-# aktivacija virtualnog okruženja
-source venv-mpi4py/bin/activate
+# aktivirati virtualno okruženje
 
-# instalirati mpi4py sa pipom 
-pip install mpi4py
+# instalirati mpi4py
 
-# deaktivacija virtualnog okruženja
-deactivate
+# deaktivirati virtualno okruženje
