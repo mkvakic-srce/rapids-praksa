a45b3d28961d110f813c2886a57d01222576db47
Jasmin Husić <jhsc1988@gmail.com>
2022-06-17 08:44:40 +0200

    3. zadatak

--- zadatak3/install-rapids.sh
+++ zadatak3/install-rapids.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # podigni CUDA knjižnicu
+module load cuda/11-2
 
 # aktiviraj condu
+source /apps/miniforge3/bin/activate
 
 # stvori virtualno okruženje za rapids
+conda create -n rapids-22.04 -c rapidsai -c nvidia -c conda-forge rapids=22.04 python=3.9 cudatoolkit=11.2
