d6e663da855416887a42895a793de07e7777c4e9
Jasmin Husić <jhsc1988@gmail.com>
2022-06-20 08:10:51 +0200

    zadatak3

--- zadatak3/double-rapids.py
+++ zadatak3/double-rapids.py
@@ -1,20 +1,22 @@
 # podignuti cudf knjižnicu
 import cudf
+import numpy as np
 
-# pomnozi listu brojeva s dva
-def double(x):
-    return x*2
-
+def double(x, out):
+    for i in range(len(x)):
+        out[i] = x[i]*2
+        
 if __name__ == '__main__':
 
     # stvori cudf Dataframe i napravi stupac brojeva
     n = 10**8
-    df = cudf.DataFrame(list(range(0, n)))
-
+    d = {'input' : list(range(0, n))}
+    df = cudf.DataFrame(data=d)
+    
     # stvori stupac udvostrucenih brojeva
     # incols - A list of names of input columns that match the function arguments
     # outcols - A dictionary of output column names and their dtype.
-    dcudf = df.apply_rows(double, incols=[df], outcols=dict(o1=np.int32))
+    dcudf = df.apply_rows(double, incols=['input'], outcols={'out' : np.int32}, kwargs={})
 
     # isprintaj prvih deset brojeva u DataFrameu
-    print(dcudf[:10]) # cudf varijanta
+    print(dcudf[:10]) 
